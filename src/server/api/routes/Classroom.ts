fc737e724ef5ddc2603f713f5ced28aadebe4c59
Panthr75 <jjireland@stu.naperville203.org>
2022-01-06 21:17:57 -0600

    Classroom updates

--- src/server/api/routes/Classroom.ts
+++ src/server/api/routes/Classroom.ts
@@ -26,7 +26,7 @@ type ClassroomSettings = {
 
 export default class ClassroomModel extends APIModel<ClassroomModel> {
 
-    private _classroomDB: DataBase<string, ClassroomSettings>;
+    private _classroomDB: DataBase<string, Classroom>;
 
     public constructor(controller: APIController) {
         // api version 1
@@ -37,5 +37,122 @@ export default class ClassroomModel extends APIModel<ClassroomModel> {
 
     protected override initRoutes(router: Router): void {
         
+        // handles requesting for a classroom via a code
+        // todo: maybe hide or change some things for the response (e.g. student emails)
+        router.get("/:code", async (req, res) => {
+            this.logger.debug("Received request for classroom with code " + req.params.code);
+            let classroom: Classroom;
+            try {
+                classroom = await this._classroomDB.get(req.params.code);
+            }
+            catch (err) {
+                // err is type any because promises can reject with any value.
+                // check if it's an actual error object
+                if (err instanceof Error) {
+                    // send fail api response
+                    res.status(404).send({
+                        "message": "Error fetching class: " + err.message,
+                        "success": false
+                    });
+
+                    return; // prevent other code from running
+                }
+                // otherwise convert to string
+                let message = new String(err);
+
+                // send fail api response
+                res.status(404).send({
+                    "message": message,
+                    "success": false
+                });
+                return; // prevent other code from running
+            }
+
+            // success api response (we may need to change it)
+            let result: Classroom = classroom;
+
+            res.send({
+                "data": result,
+                "success": true
+            });
+        });
+
+        router.get("/:code/submissions", async (req, res) => {
+            this.logger.debug("Received request for classroom submission status with code " + req.params.code);
+            let classroom: Classroom;
+            try {
+                classroom = await this._classroomDB.get(req.params.code);
+            }
+            catch (err) {
+                // err is type any because promises can reject with any value.
+                // check if it's an actual error object
+                if (err instanceof Error) {
+                    // send fail api response
+                    res.status(404).send({
+                        "message": "Error fetching class: " + err.message,
+                        "success": false
+                    });
+
+                    return; // prevent other code from running
+                }
+                // otherwise convert to string
+                let message = new String(err);
+
+                // send fail api response
+                res.status(404).send({
+                    "message": message,
+                    "success": false
+                });
+                return; // prevent other code from running
+            }
+
+            // success api response (we may need to change it)
+            let result: boolean = classroom.settings.allowSongSubmission;
+
+            res.send({
+                "data": result,
+                "success": true
+            });
+        });
+
+        router.patch("/:code/submissions", async (req, res) => {
+            this.logger.debug("Received request for setting classroom submission status with code " + req.params.code);
+
+            let classroom: Classroom;
+            try {
+                classroom = await this._classroomDB.get(req.params.code);
+            }
+            catch (err) {
+                // err is type any because promises can reject with any value.
+                // check if it's an actual error object
+                if (err instanceof Error) {
+                    // send fail api response
+                    res.status(404).send({
+                        "message": "Error fetching class: " + err.message,
+                        "success": false
+                    });
+
+                    return; // prevent other code from running
+                }
+                // otherwise convert to string
+                let message = new String(err);
+
+                // send fail api response
+                res.status(404).send({
+                    "message": message,
+                    "success": false
+                });
+                return; // prevent other code from running
+            }
+
+            classroom.settings.allowSongSubmission = req.query.enabled == "true"; 
+            // success api response (we may need to change it)
+            let result: boolean = await this._classroomDB.set(req.params.code, classroom);
+
+            res.send({
+                "data": result,
+                "success": true
+            });
+        })
     }
 }
